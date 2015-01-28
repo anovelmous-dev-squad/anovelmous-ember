b213d368223f3d2921b1129ed335de437a26ec7d
timmartin19 <tim.martin@vertical-knowledge.com>
2015-01-27 20:50:09 -0500

    live input stylings

--- app/views/live-input.js
+++ app/views/live-input.js
@@ -2,7 +2,31 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
   tagName: 'input',
+  attributeBindings: ['value'],
   classNames: ['live-input'],
-  classNameBindings: ['error'],
-  error: false
+  classNameBindings: ['error', 'voted', 'success', 'failure', 'nothing'],
+  error: false,
+  voted: false,
+  success: false,
+  failure: false,
+  nothing: function(){
+    return (this.get('error') === this.get('voted') === this.get('success') === false);
+  }.property('error', 'voted', 'success'),
+  keyUp: function(event){
+    if(event.keyCode == 13){  // return key pressed
+      if(!this.get('error')){
+        this.submitVote();
+        // TODO flash messages
+      }
+      else{
+        // TODO
+      }
+    }
+  },
+  submitVote: function(){
+    // TODO
+  },
+  validateValue: function(){
+    // TODO figure out how to validate value
+  }.observes('value')
 });
